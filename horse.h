95c049cc24b7e7e65bfd2cce3e9cf2ab90411641
Logan1allen <logan.allen@bsu.edu>
2025-02-07 14:28:32 +0000

    Added Horse.h

---
+++ horse.h
@@ -0,0 +1,16 @@
+#ifndef HORSE_H_INCLUDED
+#define HORSE_H_INCLUDED
+
+class horse{
+    private:
+        int id;
+        int position;
+        int trackLength;
+    
+    public:
+        Horse();
+        void init(int id, int trackLength);
+        void advance();
+        printLane();
+        bool isWinner();
+}
